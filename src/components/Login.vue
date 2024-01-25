575c2bce6d48150f38b0676cddb8797989c4921d
mahbub <mahbub.mononsoft@gmail.com>
2024-01-25 13:09:27 +0600

    api itriget

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,14 +8,15 @@
  
       <form action="#" @submit.prevent="handleSubmit">
         <label class="block" style="display:table">Email</label>
-        <input type="email" placeholder="Enter your email" 
-        v-model="fromData.email" ref="email"/>
+        <input type="text" placeholder="Enter your username" 
+        v-model="fromData.username" ref="username"/>
 
         <label class="block mt-3" style="display:table">Password</label>
         <input type="password" placeholder="Enter password" 
         v-model="fromData.passwoard" required ref="passwoard"/>
 
-        <button type="submit" class="w-100 mt-3">Login</button>
+        <p class="text-center mt-3" v-if="loggingIn">Logging in...</p>
+        <button type="submit" class="w-100 mt-3" v-else>Login</button>
 
         <div class="d-flex jc-between mt-3">
           <div>
@@ -35,24 +36,28 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data(){
     return {
           fromData:{
-              email:"",
+              username:"",
               passwoard:""
-          }
+          },
+          loggingIn: false,
+          movedToRight: false,
+          showing: false
     }
   },
   methods: {
     handleSubmit(){
-      if(!this.fromData.email){
-        // alert('entry your email')
+      if(!this.fromData.username){
+        // alert('entry your username')
         this.$eventBus.emit("toast",{
           type:"Error",
-          message:"entry your email"
+          message:"entry your username"
         })
-        this.$refs.email.focus();
+        this.$refs.username.focus();
         return
       }
       if(this.fromData.passwoard.length<4){
@@ -64,7 +69,32 @@ export default {
         this.$refs.passwoard.focus();
         return
       }
-      console.log('...submit..sucess..')
+      // console.log('...submit..sucess..')
+      //  console.log(this.fromData);
+      this.loggingIn = true;
+      axios.post("https://api.rimoned.com/api/pharmacy-management/v1/login",
+        this.fromData
+      )
+      .then((res)=>{
+        console.log(res.data);
+        this.$eventBus.emit("toast",{
+          type: "Success",
+          message: res.data.message
+        });
+      })
+      .catch((err)=>{
+          let errMessage ="Something Wrong.!"
+          if(err.response){
+            errMessage = err.response.data.message
+          }
+          this.$eventBus.emit("toast",{
+            type: "Error",
+          message: errMessage
+          })
+      })
+      .finally(()=>{
+          this.loggingIn=false;
+      });
     }
   },
 };
@@ -113,7 +143,7 @@ export default {
   padding: 44px 33px;
 }
 
-.login-card input[type="email"],
+.login-card input[type="text"],
 .login-card input[type="password"] {
   width: 100%;
 }
@@ -140,30 +170,30 @@ export default {
 } */
 
 .showhide-enter-active {
-  /* animation: showhide 1s ease-in; */
+  animation: showhide 1s ease-in;
   /* transition: all 0.5s; */
 }
 .showhide-enter-from {
-  /* opacity: 0;
-  transform: scale(0.5); */
+  opacity: 0;
+  transform: scale(0.5);
 }
 
 .showhide-enter-to {
-  /* opacity: 1;
-  transform: scale(1); */
+  opacity: 1;
+  transform: scale(0.5);
 }
 
 .showhide-leave-active {
-  /* animation: showhide 1s ease-in reverse; */
+  animation: showhide 1s ease-in reverse;
   /* transition: all 0.5s; */
 }
 .showhide-leave-from {
-  /* opacity: 1;
-  transform: scale(1); */
+  opacity: 1;
+  transform: scale(1);
 }
 
 .showhide-leave-to {
-  /* opacity: 0;
-  transform: scale(0.5); */
+  opacity: 0;
+  transform: scale(1);
 }
 </style>
